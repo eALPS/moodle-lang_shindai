d3281ed37e72731897a8ef7c5eb0573084ad294e
Osamu HASEGAWA <good.old.memories.quoll@gmail.com>
2012-11-02 13:17:14 +0900

    クイックメールの言語パック

---
+++ block_quickmail.php
@@ -0,0 +1,74 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Local language pack from http://moodle-rc.ealps.shinshu-u.ac.jp/2012/mdl23-git
+ *
+ * @package    block
+ * @subpackage quickmail
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['add_all'] = '全て追加';
+$string['add_button'] = '追加';
+$string['all_sections'] = '全てのセクション';
+$string['allowstudents'] = '学生にクイックメールを使用することを許可する';
+$string['alternate_from'] = 'Moodle：クイックメール';
+$string['are_you_sure'] = '{$a->title} を削除してもよいですか？ この操作は取り消すことができません．';
+$string['attachment'] = '添付';
+$string['backup_history'] = 'クイックメールの履歴を含める';
+$string['composenew'] = '新しいメールを作成する';
+$string['config'] = '設定';
+$string['courselayout'] = 'コースレイアウト';
+$string['default_flag'] = 'デフォルト';
+$string['delete_confirm'] = 'このメッセージを削除してもよいですか？：{$a}';
+$string['delete_failed'] = 'メールの削除に失敗しました';
+$string['download_all'] = '全てダウンロード';
+$string['drafts'] = '下書きを見る';
+$string['email'] = 'メール';
+$string['from'] = '差出人';
+$string['history'] = '履歴を見る';
+$string['log'] = '履歴を見る';
+$string['message'] = '本文';
+$string['no_drafts'] = 'メールの下書きがありません．';
+$string['no_filter'] = 'フィルタなし';
+$string['no_log'] = 'まだメールの履歴がありません．';
+$string['no_subject'] = '件名を入力してください．';
+$string['overwrite_history'] = 'クイックメールの履歴を上書きする';
+$string['pluginname'] = 'クイックメール';
+$string['potential_users'] = '潜在的なユーザ';
+$string['prepend_class'] = 'メールの先頭にコース名を挿入する';
+$string['prepend_class_desc'] = 'メールの件名にコースの省略名称を入れる';
+$string['quickmail:canconfig'] = 'ユーザにクイックメールの設定を行うことを許可する．';
+$string['quickmail:cansend'] = 'ユーザにクイックメールを使って送信することを許可する．';
+$string['receipt_help'] = '送信したメールのコピーを受け取る';
+$string['remove_all'] = '全て削除';
+$string['remove_button'] = '削除';
+$string['required'] = '必須項目を入力してください．';
+$string['reset'] = 'システムデフォルトに戻す';
+$string['restore_history'] = 'クイックメールの履歴をリストアする';
+$string['role_filter'] = 'ロールフィルタ';
+$string['save_draft'] = '下書きを保存';
+$string['select_groups'] = 'セクションを選択 ...';
+$string['select_users'] = 'ユーザを選択 ...';
+$string['send_email'] = 'メールを送信';
+$string['sig'] = '署名';
+$string['signature'] = '署名を追加する';
+$string['subject'] = '件名';
+$string['title'] = 'タイトル';
